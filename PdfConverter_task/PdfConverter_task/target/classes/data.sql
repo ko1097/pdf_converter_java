183cd3c5e60f034eaeea86c82e53fefeafd6909f
ko1097 <67546597+ko1097@users.noreply.github.com>
2025-09-22 15:13:01 +0530

    Delete PdfConverter_task/PdfConverter_task/target directory

--- PdfConverter_task/PdfConverter_task/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO BOOK (RollNo,Name,MATHS,Science,Social,Total,Result) VALUES (120,'Ko',85, 88, 75,270,'Pass');
-INSERT INTO BOOK (RollNo,Name,MATHS,Science,Social,Total,Result) VALUES (121,'SH',45, 88, 65,250,'Pass');
-INSERT INTO BOOK (RollNo,Name,MATHS,Science,Social,Total,Result) VALUES (122,'ik',25, 18, 25,170,'Fail');
-INSERT INTO BOOK (RollNo,Name,MATHS,Science,Social,Total,Result) VALUES (123,'rk',97, 98, 95,290,'Pass');
